58c3888332ca1da1aef3afa9833ff9c496c23a2c
hilusjr <pietrzef@gmail.com>
2025-01-24 16:48:53 +0100

    adapt to 1920x1080

--- script/panel.js
+++ script/panel.js
@@ -1,3 +1,6 @@
+const openPanel = document.querySelector('.panel-button')
+const closePanel = document.querySelector('.close-panel')
+const panel = document.querySelector('.panel')
 const addButton = document.querySelector('.add-button')
 const addSelectionContainer = document.querySelector('.add-selection-container')
 const trafficLightsSelection = document.querySelector(
@@ -5,6 +8,14 @@ const trafficLightsSelection = document.querySelector(
 )
 const removeLightsButton = document.querySelector('.remove-lights-button')
 const setTimingButton = document.querySelector('.set-timing-button')
+
+openPanel.addEventListener('click', () => {
+  panel.style.display = 'flex'
+})
+
+closePanel.addEventListener('click', () => {
+  panel.style.display = 'none'
+})
 
 addButton.addEventListener('click', () => {
   addSelectionContainer.style.display = 'flex'
